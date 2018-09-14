458ff525b4ee34ea38906be52d84270fc0894c42
ggr <ginagonzalezroundey@gmail.com>
2018-09-14 12:13:22 -0400

    updated server to read js and css files +  html tags

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ const url  = require('url');
 const port = 8080;
 
 var server = http.createServer (function (req, res) {
-	fs.readFile('index.html', function(error, content) {
-		res.writeHead(200, {'Content-type': 'text/html'})
+	var path = url.parse(req.url).pathname;
+	switch (path) {
+		case '/':
+		case '/index.html':
+			read('index.html', res, 'text/html');
+		case '/style.css':
+			read('style.css', res, 'text/css');
+		case '/scripts.js':
+			read('scripts.js', res, 'text/javascript');
+	}
+});
+
+function read(path, res, contentType) {
+	fs.readFile(path, function(error, content) {
+		res.writeHead(200, {'Content-type': contentType})
 		res.end(content, 'utf-8')
 	});
-});
+}
 
 server.listen(process.env.PORT || port);
 console.log('listening on http://localhost:8080');
